e68f4ca895236d1fc66a41be391e1882649f9261
Jandrik <jandrik.sachau@gmail.com>
2020-11-24 18:25:13 +0100

    Alles was bei mir bisher geschah

--- adminpub/poker/backend/models/game_class.js
+++ adminpub/poker/backend/models/game_class.js
@@ -1,7 +1,12 @@
 class Game {
   constructor(players) {
     this.players = players;
-  }
+    /*  TODO:
+        evtl. Array mit allen Spielern, wenn nicht dann:
+            muss eins erstellt werden
+        + alle this.players kontrollieren
+    */
+    }
 
   running = false;
 
@@ -10,6 +15,85 @@ class Game {
      
     // Why does this go through? Prolly too fast?
     console.log("Das Spiel hat begonnen");
+  }
+
+
+  static dealt = []; // Ausgeteilte Karten
+
+  static rndInt (max, min) {
+      return Math.floor(Math.random() * max) + min;
+  }
+
+  static getCard () {  // Gib eine noch nich ausgeteilte Card zurück
+    let rückgabe = [];     
+    let card = this.rndInt(52, 1);
+
+    while (this.dealt.includes(card)) {
+        card = this.rndInt( 52, 1);
+    }
+
+    this.dealt.push(card);
+    
+    rückgabe[0] = 0;
+    while (card > 13) {
+        card -= 13;
+        if ( card > 0) rückgabe[0] ++;
+    }
+    switch (rückgabe[0]) {
+        case 0:
+            rückgabe[0] = "Kreutz";
+            break;
+        case 1:
+            rückgabe[0] = "Pik";
+            break;
+        case 2:
+            rückgabe[0] = "Herz";
+            break;
+        case 3:
+            rückgabe[0] = "Karo";
+            break;
+        default:
+            break;
+    }
+
+    rückgabe[1] = card + 1;
+
+    return new Card( rückgabe[0], rückgabe[1]);
+  }
+
+  /* Phasen des Spiels:
+  prefloop
+  flop
+  turn
+  river
+  */
+  static preflop () { // Gibt jedem Spieler zwei Karten
+      for (let j of this.players) {
+          j.cards.push(this.getCard());
+          j.cards.push(this.getCard());
+      }
+  }
+  
+  static cardOnTable () { // Legt eine Karte auf den Tisch
+      let tableCard = this.getCard();
+
+      for (let j of player.this) {
+          j.cards.push(tableCard);
+      }
+  }
+
+  static flop () {    // Phase des Spiels
+      for (let i = 0; i < 3; i++) {
+          this.cardOnTable();
+      }
+  }
+
+  static turn () {    // Phase des Spiels
+      this.cardOnTable();
+  }
+
+  static river () {   // Phase des Spiels
+      this.cardOnTable();
   }
 }
 
---
+++ adminpub/poker/backend/models/player_class.js
@@ -0,0 +1,178 @@
+class Player {
+    constructor (name) {
+        this.name = name;
+    }
+    chips = 5000;
+    cards = [];
+
+    numArrayContains (array, index) {   // Gibt an wie oft der "index" im array[] vorhanden ist
+        var n = 0;
+        for (let i of array) {
+            if ( i == index) n++;
+        }
+
+        return n;
+    }
+
+    typeAmount (type) {
+        /* type ist "color" o. "number"
+            gibt Array zurück mit:
+                [0][i] den verschiedenen Farben/Nummern
+                [1][i] der Anzahl wie oft diese in player.cards[] vorkommt
+                [2] Farbe des Flushs, wenn vorhanden
+        */
+        var rückgabe = [ [], [] ];
+        for (let i of this.cards ) {
+            if (rückgabe[0].indexOf(i[type]) !== -1) {
+                rückgabe[1][rückgabe[0].indexOf(i[type])] ++;
+            } else {
+                rückgabe[0].push(i[type]);
+                rückgabe[1][rückgabe[0].indexOf(i[type])] = 1;
+            }
+        }
+
+        for (let c = 5; c <= 7; c ++) {
+            if ( rückgabe[1].includes(c)) rückgabe[2] = rückgabe[0][rückgabe[1].indexOf(c)];
+        }
+
+        return rückgabe;
+    }
+
+    getHand () {    // Gibt die Hand des Spieler in einem Array wieder
+        var cAmount = this.typeAmount("color");
+        var nAmount = this.typeAmount("number");
+        
+        const flushCards = () => {  // Array mit den Karten die im Flush sind
+            let flushCards = [];
+
+            for ( let i of this.cards) {
+                if ( i.color == cAmount[2]) {
+                    flushCards.push(i.number);
+                }
+            }
+
+            return flushCards.sort((a, b) => a - b);
+        }
+        
+        const hasStraight = (n) => {   // Gib an ob das übergebene Array > 5 aufeinander folgende Zahlen hat
+            var rückgabe = [ 1, 1];
+            var counter = 1;
+            var straightCards;
+            if (n == 1) {   // damit nAmount nicht sortiert wird; 
+                straightCards = flushCards();
+            } else {
+                straightCards = this.typeAmount("number");
+                straightCards.sort ((a, b) => a - b);
+            }
+    
+            for (let i = 0; i < straightCards.length - 1; i++) {
+    
+                if (straightCards[i] +1 == straightCards[i + 1]) {
+                    counter ++;
+                    if (counter > rückgabe[0]) {
+                        rückgabe[0] = counter;
+                        rückgabe[1] = straightCards[i + 1];
+                    } else {
+                        if (straightCards[i] !== straightCards[i +1]) counter = 1; 
+                    }
+                }
+            }
+    
+            if (rückgabe[0] < 5) {
+                rückgabe[0] = false;
+            } else {
+                rückgabe[0] = true;
+            }
+    
+            return rückgabe;  // Rückgabe[0] Zahlenfolge ist größer 5, [1] höchste Zahl der Zahlenfolge
+        }
+
+        const highCard = (c1, c2) => {  // Gibt die höchst Zahl wieder die nicht in der Hand ist (c1, c2 müssen als Karten der Hand mit übergeben werden)
+            let karten = []
+            for (let i of this.cards) {
+                if (i.number !== c1 && i.number !== c2) {
+                    karten.push(i.number);
+                }
+            }
+        
+            karten.sort((a ,b) => a - b);
+            return karten[karten.length - 1];
+        }
+
+        if (typeof(cAmount[2]) !== "undefined") {   	// Spieler hat einen Flush
+
+            if (hasStraight(1)[0]  && hasStraight(1)[1] == 14) {
+                return 10;  // Check if Royal Flush
+            } else {
+                if (hasStraight(1)[0]) return [ 9, hasStraight(1)[1]];    // Check if Sraight Flush
+            }
+
+            let fCards = flushCards();
+            while (fCards.length > 5) {
+                fCards.shift();
+            }
+
+            return [6, fCards];
+        }
+
+        if (hasStraight(0)[0]) return [5, hasStraight(0)[1]];
+
+        if (nAmount[1].includes(4)) {   // Player has 4 of a Kind
+            return [8, nAmount[0][nAmount[1].indexOf(4)], highCard(nAmount[0][nAmount[1].indexOf(4)])];
+        }
+
+        if (nAmount[1].includes(3) && nAmount[1].includes(2) || this.numArrayContains(nAmount[1], 3) == 2) {    // Player hat Full House
+            var rückgabe = [];
+            rückgabe [0] = 7;
+            var karten = [];
+
+            if (this.numArrayContains(nAmount[1], 2) == 2) {
+                for (let i = 0; i < nAmount[0].length; i ++) {
+                    if (nAmount[1][i] == 2) karten.push(nAmount[0][i]);
+                }
+                karten.sort((a ,b) => a - b);
+                rückgabe[2] = karten[karten.length - 1];
+            }
+
+            if (this.numArrayContains(nAmount[1], 3) == 2){
+                for (let i = 0; i < nAmount[0].length; i ++) {
+                    if (nAmount[1][i] == 3) karten.push(nAmount[0][i]);
+                }
+                karten.sort((a ,b) => a - b);
+                rückgabe[1] = karten[1];
+                rückgabe[2] = karten[0];
+
+                return rückgabe;
+            }
+
+            if ( typeof(rückgabe[2]) == "undefined") rückgabe[2] = nAmount[0][nAmount[1].indexOf(2)];
+            rückgabe[1] = nAmount[0][nAmount[1].indexOf(3)];
+
+            return rückgabe;
+        }
+
+        if (nAmount[1].includes(3)) {   // Player hat 3 of a Kind
+            return [4, nAmount[0][nAmount[1].indexOf(3)], highCard(nAmount[0][nAmount[1].indexOf(3)])];
+        }
+
+        if (nAmount[1].includes(2)) {
+            var karten = [];
+
+            if (this.numArrayContains(nAmount[1], 2) >= 2) {    // Two Pairs
+                
+                for (let i = 0; i < nAmount[0].length; i ++) {
+                    if (nAmount[1][i] == 2) karten.push(nAmount[0][i]);
+                }
+
+                karten.sort((a, b) => a - b);
+                if (karten.length == 3) karten.shift();
+
+                return [3, karten[1], karten[0], highCard(karten[1], karten[0])];
+            } else {    // One Pair
+                return [2, nAmount[0][nAmount[1].indexOf(2)], highCard(nAmount[0][nAmount[1].indexOf(2)])];
+            }
+        }
+        
+        return [1, highCard()];
+    }
+}
