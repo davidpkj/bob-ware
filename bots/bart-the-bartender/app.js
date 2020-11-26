fc8bd35950345ae1ebc03d8cab9005c6785aa0a2
SailingJo <59749287+SailingJo@users.noreply.github.com>
2020-11-26 19:28:45 +0100

    Update app.js

--- bots/bart-the-bartender/app.js
+++ bots/bart-the-bartender/app.js
@@ -22,12 +22,50 @@ client.on("message", (msg) => {
   }
 });
 
-const shot = (msg) => {
-  msg.reply("kommt sofort!");
+const alcohol = (msg) => {
+  switch (msg.content.toLowerCase().substring(1, msg.content.length)){
+    case "shot": 
+      msg.reply("Kommt sofort!");
+      break;
+    case "martini":
+      msg.reply("Möchten Sie ihn geschüttelt oder gerührt?");
+      break;
+    case "whiskey":
+      msg.reply("Was darf es sein? Ein irischer Single Malt, Tennessee, Bourbon oder doch Scotch?");
+      break;
+    case "wässerchen":
+    case "vodka":
+      msg.reply("За здоровье!");
+      break;
+    case "cocktail":
+      msg.reply("Sex On The Beach, Long Island Ice Tea, Piña Colada, Magerita, Moscow Mule oder ein Tequila Sunrise? Wonach ist Ihnen?");
+      break;
+    case "rum":
+      msg.reply("Ein deutscher Meyer's, der venezuelanische Botucal oder womöglich der Plantation Barbados Extra Old? Ein Kenner ist sich der Qualitäten bewusst.");
+      break;
+    case "gin":
+      msg.reply("Eine Gin Tonic auf Eis, kommt sofort!");
+      break;
+    case "wein":
+      msg.reply("Lieblichen und Rosé gibt es hier nicht. Wir haben ungarischen Stierblut, Riesling, Cuvée Especial und den Châteauneuf-du-Pape Les Sinards");
+      break;
+    case "drinks":
+      msg.reply("Ich kann hiermit dienen: Martini, Whiskey, Gin, Vodka, Rum, Wein, Coacktail und einem Shot")
+      break;
+  }
 }
 
 const commands = {
-  "shot": shot,
+  "shot": alcohol,
+  "martini": alcohol,
+  "whiskey": alcohol,
+  "gin": alcohol,
+  "wässerchen": alcohol,
+  "cocktail": alcohol,
+  "vodka": alcohol,
+  "rum": alcohol,
+  "wein": alcohol,
+  "drinks": alcohol,
 }
 
 const unknownCommand = (msg) => {
