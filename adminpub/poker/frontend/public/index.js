8b82f01dfba2f3ca0242fd37deb8905dbe65fd0c
Jandrik <jandrik.sachau@gmail.com>
2021-01-31 18:48:49 +0100

    Grobes Dashboard and Pokerlobby design

--- adminpub/poker/frontend/public/index.js
+++ adminpub/poker/frontend/public/index.js
@@ -1,2 +1,10 @@
 var socket = io();
-// erugtlf
+
+const join = () => {    // tut den Namen nach submit in pList
+    const name = document.getElementById("playerName").value;
+    document.getElementById("playerName").value = "";
+
+    const p = document.createElement("p");
+    p.innerHTML = name;
+    document.getElementById("pList").appendChild(p);
+};
